c73ef54682fcd4492a7d3c3591220f4a3d48e1fd
allisonwong1228 <allisonwong1228@gmail.com>
2022-01-03 23:42:50 -0500

    first commit

---
+++ lol.R
@@ -0,0 +1,6 @@
+library(distill)
+create_post("HOW TO START VIDEO EDITING?", slug = "HOW TO START VIDEO EDITING?")
+create_post("SMOOTH CLIPS/VELOCITY ON CLIPS", slug = "SMOOTH CLIPS/VELOCITY ON CLIPS")
+usethis::use_git() 
+2
+2
