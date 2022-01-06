44818c4bff5d55fe211b401e5788127a551292a4
allisonwong1228 <allisonwong1228@gmail.com>
2022-01-06 00:59:38 -0500

    commit 6

--- lol.R
+++ lol.R
@@ -4,3 +4,15 @@ create_post("SMOOTH CLIPS/VELOCITY ON CLIPS", slug = "SMOOTH CLIPS/VELOCITY ON C
 usethis::use_git() 
 2
 2
+usethis::use_github()
+11
+1
+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
+usethis::create_github_token()
+gitcreds::gitcreds_set()
+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
+3
+create_theme(name = lwo_theme)
+install.packages("distill")
+create_theme(name = "lwo_theme")
+create_post("MY FAVORITE AFFIRMATIONS", slug = "MY FAVORITE AFFIRMATIONS")
